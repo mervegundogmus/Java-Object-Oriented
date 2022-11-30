cec74dd110dff27d006f4681291588c3b53ee5d1
Merve <mervegundogmus@outlook.com>
2022-11-30 20:46:31 +0300

    Encapsulation

--- src/com/example/Objects/Tree.java
+++ src/com/example/Objects/Tree.java
@@ -3,29 +3,29 @@ package com.example.Objects;
 import java.awt.*;
 
 public class Tree {
-    double heightFt;
-    double trunkDiameterInches;
-    TreeType treeType;
-    static Color TRUNK_COLOR = new Color(102, 51, 0);
 
+    protected static Color trunkColor = new Color(102, 51, 0);
+    private double heightFt;
+    private double trunkDiameterInches;
+    private TreeType treeType;
 
-    Tree(double heightFt, double trunkDiameterInches,
+    public Tree(double heightFt, double trunkDiameterInches,
          TreeType treeType){
         this.heightFt = heightFt;
         this.trunkDiameterInches = trunkDiameterInches;
         this.treeType = treeType;
     }
 
-    void grow(){
+    public void grow(){
         this.heightFt = this.heightFt + 10;
         this.trunkDiameterInches = this.trunkDiameterInches + 1;
     }
 
-    static void announceTree() {
+    public static void announceTree() {
         System.out.println("Look out for that " +
                 TRUNK_COLOR + "tree!");
     }
-    void announceTallTree(){
+    public void announceTallTree(){
         if (this.heightFt > 100) {
             System.out.println("That's a tall " +
                     this.treeType + " tree!");
