2e47f0fd8d920d33910dcd0c4b402603e2f20ab1
Merve <mervegundogmus@outlook.com>
2022-12-02 01:12:10 +0300

    Update

--- src/com/example/Polymorphism/Main.java
+++ src/com/example/Polymorphism/Main.java
@@ -10,25 +10,37 @@ public class Main {
         Random random = new Random();
         while (originalSize + 1 != list.size()) {
             int n = random.nextInt() % 1000;
+
+            if (list instanceof ConditionArrayList) {
+                ConditionArrayList conditionArrayList = (ConditionArrayList) list;
+                while (!conditionArrayList.isEligible(n)) {
+                    n = random.nextInt(1000);
+                }
+            }
             list.add(n);
         }
     }
 
     public static void main(String[] args) {
 
-        OddArrayList oddListy = new OddArrayList(7);
+        ConditionArrayList oddListy = new ConditionArrayList(
+                n -> Math.abs(n) % 2 == 1);
         oddListy.add(1);
         oddListy.add(2);
         addRandomNumber(oddListy);
         System.out.println(oddListy);
-//      System.out.println(oddListy.size());
+
+        ConditionArrayList evenListy = new ConditionArrayList(
+                n -> Math.abs(n) % 2 == 0);
+        evenListy.add(1);
+        evenListy.add(2);
+        addRandomNumber(evenListy);
+        System.out.println(evenListy);
 
         ArrayList<Integer> listy = new ArrayList<>();
         listy.add(1);
         listy.add(2);
         addRandomNumber(listy);
         System.out.println(listy);
-//      System.out.println(listy.size());
     }
-
 }
