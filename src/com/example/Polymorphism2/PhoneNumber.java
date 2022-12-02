eda16a97d3d51d73cf391f1c7f0f4c8dff92621d
Merve <mervegundogmus@outlook.com>
2022-12-02 22:08:04 +0300

    Update

--- src/com/example/Polymorphism2/PhoneNumber.java
+++ src/com/example/Polymorphism2/PhoneNumber.java
@@ -1,4 +1,15 @@
 package com.example.Polymorphism2;
 
 public class PhoneNumber {
+    private int countryCode;
+    private String phoneNumber;
+
+    public PhoneNumber(String phoneNumber) {
+
+    }
+
+    public PhoneNumber(int countryCode, String phoneNumber) {
+        this.countryCode = countryCode;
+        this.phoneNumber = phoneNumber;
+    }
 }
